94815f8050f1a477c08cc8bf22d26a0a445aa845
Jareer Abdullah <jforeverything2007@gmail.com>
2022-06-28 21:27:26 +0200

    update

--- src/components/library/MyPage.vue
+++ src/components/library/MyPage.vue
@@ -18,7 +18,7 @@ export default {
         <section class="">
           <main>
             <div>
-              <h2 class="text-xl font-semibold">Select Theme</h2>
+              <h2 class="text-xl font-semibold">Basic Information</h2>
             </div>
             <div class="gap-3 mt-3 grid-cols-2 grid">
               <section>
@@ -28,12 +28,82 @@ export default {
                       flex flex-wrap
                       w-full
                       items-center
-                      space-x-3
+                      space-y-3
                       bg-steel-400
                       p-3
                       rounded-lg
                     "
-                  ></div>
+                  >
+                    <div class="w-full">
+                      <label for="placeholder-gray-300">Username</label>
+                      <input
+                        type="text"
+                        class="
+                          bg-steel-300
+                          placeholder-gray-400
+                          p-3
+                          rounded-lg
+                          w-full
+                        "
+                        placeholder="John Doe"
+                      />
+                    </div>
+                    <div class="w-full">
+                      <label for="placeholder-gray-300">Contact Email</label>
+                      <input
+                        type="text"
+                        class="
+                          bg-steel-300
+                          placeholder-gray-400
+                          p-3
+                          rounded-lg
+                          w-full
+                        "
+                        placeholder="johndoe@example.com"
+                      />
+                    </div>
+                    <div class="w-full">
+                      <label for="placeholder-gray-300">Contact Email</label>
+                      <textarea
+                        type="text"
+                        class="
+                          bg-steel-300
+                          placeholder-gray-400
+                          p-3
+                          rounded-lg
+                          w-full
+                        "
+                        placeholder="No description given."
+                      ></textarea>
+                    </div>
+                  </div>
+                  <div
+                    class="
+                      mt-3
+                      flex flex-wrap
+                      w-full
+                      items-center
+                      space-y-3
+                      bg-steel-400
+                      p-3
+                      rounded-lg
+                    "
+                  >
+                    <div class="w-full">
+                      <label for="placeholder-gray-300">Username</label>
+                      <input
+                        type="text"
+                        class="
+                          bg-steel-300
+                          placeholder-gray-400
+                          p-3
+                          rounded-lg
+                          w-full
+                        "
+                        placeholder="John Doe"
+                      />
+                    </div>
+                  </div>
                 </main>
               </section>
               <section>
