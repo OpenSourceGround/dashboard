92d3b70fdd25e9b219249bc9a1cfa85edb17fb8b
Jareer Abdullah <jforeverything2007@gmail.com>
2022-06-29 20:36:28 +0200

    Done for today

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -58,6 +58,56 @@ export default {
             ],
           },
         },
+        radar: {
+          options: {
+            maintainAspectRatio: false,
+            scales: {
+              y: {
+                display: false,
+                beginAtZero: true,
+              },
+              x: {
+                display: false,
+              },
+            },
+          },
+          data: {
+            labels: [
+              "Eating",
+              "Drinking",
+              "Sleeping",
+              "Designing",
+              "Coding",
+              "Cycling",
+              "Running",
+            ],
+
+            datasets: [
+              {
+                label: "My First Dataset",
+                data: [65, 59, 90, 81, 56, 55, 40],
+                fill: true,
+                backgroundColor: "rgba(255, 99, 132, 0.2)",
+                borderColor: "rgb(255, 99, 132)",
+                pointBackgroundColor: "rgb(255, 99, 132)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgb(255, 99, 132)",
+              },
+              {
+                label: "My Second Dataset",
+                data: [28, 48, 40, 19, 96, 27, 100],
+                fill: true,
+                backgroundColor: "rgba(54, 162, 235, 0.2)",
+                borderColor: "rgb(54, 162, 235)",
+                pointBackgroundColor: "rgb(54, 162, 235)",
+                pointBorderColor: "#fff",
+                pointHoverBackgroundColor: "#fff",
+                pointHoverBorderColor: "rgb(54, 162, 235)",
+              },
+            ],
+          },
+        },
         dough: {
           options: {
             maintainAspectRatio: false,
@@ -124,13 +174,21 @@ export default {
             :options="charts.ram.options"
           />
         </div>
-        <section class="flex flex-wrap w-full col-span-3">
+        <section class="flex flex-wrap w-full col-span-3 space-x-3">
           <div>
             <NewChart
               type="doughnut"
               :header="`Ram Usage`"
               :data="charts.dough.data"
               :options="charts.dough.options"
+            />
+          </div>
+          <div>
+            <NewChart
+              type="radar"
+              :header="`Ram Usage`"
+              :data="charts.radar.data"
+              :options="charts.radar.options"
             />
           </div>
         </section>
